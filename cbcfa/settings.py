281866cc1c7de34548ae423273985c0b1960fa5c
NDICSONLABS <ndicsonlabs@gmail.com>
2021-12-28 12:26:50 +0100

    modify the release phase

--- cbcfa/settings.py
+++ cbcfa/settings.py
@@ -118,7 +118,7 @@ DATABASES['default'].update(dj_database_url.config(conn_max_age=500, ssl_require
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Allow all host headers
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://cbc-fa.herokuapp.com/','cbc-fa.herokuapp.com']
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
